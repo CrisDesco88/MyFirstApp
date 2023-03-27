e03b55e833e9e254a7730440ae33b413d1060b95
Usuario <cristiandescosido@gmail.com>
2023-03-27 18:04:39 -0300

    wip

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿// Ejercicio 1 Escribe un programa que reciba tu nombre y lo escriba por consola.
 
-Console.WriteLine("Introduce tu nombre:");
-string nombre = Console.ReadLine();
-Console.WriteLine("Hola " + nombre);
+//Console.WriteLine("Introduce tu nombre:");
+//string nombre = Console.ReadLine();
+//Console.WriteLine("Hola " + nombre);
 
 
 //Ejercicio 2 Escribe un programa que tome la hora y la escriba por pantalla.
 
-Console.WriteLine("Introduce la hora: ");
-DateTime dateTime = DateTime.Parse(Console.ReadLine());
-Console.WriteLine(dateTime);
+//Console.WriteLine("Introduce la hora: ");
+//DateTime dateTime = DateTime.Parse(Console.ReadLine());
+//Console.WriteLine(dateTime);
+
+using MyFirstApp;
+
+MyFirstApp.Variables();
---
+++ Variables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Variables
+    {
+        public Variables() {
+            int numero = 0;
+            Console.WriteLine("Introduce tu nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Intorduce tu ciudad: ");
+            string ciudad = Console.ReadLine();
+            Console.WriteLine("Intoduce tu edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Hola " + nombre);
+            Console.Write("Vives en " + ciudad);
+            Console.Write("Y tu edad es: " +  edad);
+
+        
+        }
+    }
+}
